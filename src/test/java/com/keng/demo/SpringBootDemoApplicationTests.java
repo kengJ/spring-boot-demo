300de8b30b068f43593c00553c72f37f97911ec8
kengJ <xheiyu@126.com>
2018-11-06 17:59:41 +0800

    update

--- src/test/java/com/keng/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/keng/demo/SpringBootDemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.keng.demo;
 import com.keng.demo.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ImportResource;
@@ -20,9 +22,19 @@ public class SpringBootDemoApplicationTests {
     @Autowired
     Person person;
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void contextLoads() {
         System.out.print(person);
     }
 
+    public void logTest(){
+        // 日志的级别，由低到高
+        logger.trace("trace日志");
+        logger.debug("debug日志");
+        logger.info("info日志");
+        logger.warn("warn日志");
+        logger.error("error日志");
+    }
 }
